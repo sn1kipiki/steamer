51d02dcc54c8190241b4987cc484263ce0dcd5fb
sn1kipiki <yurazayarnuk@gmail.com>
2024-02-10 00:05:37 +0200

    superchat bug fix

--- src/api/telegram.js
+++ src/api/telegram.js
@@ -1,14 +1,15 @@
-const baseUrl = "https://api.telegram.org/bot6710199081:AAGzcSbu5Tin7E30yQuAVyul1T2xAlBdKng/"
+const baseUrl =
+	'https://api.telegram.org/bot6710199081:AAGzcSbu5Tin7E30yQuAVyul1T2xAlBdKng/'
 
-export const sendMessage = async (message) =>{
-    const url = `${baseUrl}sendMessage?chat_id=-4199253673&text=${message}`
+export const sendMessage = async message => {
+	const url = `${baseUrl}sendMessage?chat_id=-4168824629&text=${message}`
 
-    const response = await fetch(url)
+	const response = await fetch(url)
 
-    if(response.ok === false){
-        const error = await response.json();
+	if (response.ok === false) {
+		const error = await response.json()
 
-        await Promise.reject(error.description || "Some errors")
-    }
-    console.log("response", response)
+		await Promise.reject(error.description || 'Some errors')
+	}
+	console.log('response', response)
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,71 +1,67 @@
 import React from 'react'
-import { useNavigate } from "react-router-dom";
-import { Carousel } from 'react-responsive-carousel';
-import instagramImg from '../img/instagram-fill.svg'
-import telegramImg from '../img/telegram-fill.svg'
-import facebookImg from '../img/facebook-circle-fill.svg'
+import { Carousel } from 'react-responsive-carousel'
+import { useNavigate } from 'react-router-dom'
+import DescriptionTabs from '../components/DescriptionTabs'
 import Img_2 from '../img/img-2.jpg'
 import Img_3 from '../img/img-3.jpg'
 import Img_4 from '../img/img-4.jpg'
 import Img_5 from '../img/img-5.jpg'
-import DescriptionTabs from '../components/DescriptionTabs';
 function Main() {
-  const navigate = useNavigate();
-  const navigationHandler = () => {
-    
-        navigate("/form");
-  
-};
-  return (
-    <div>
-              
-
-         <div className="header">
-        <a href="/#"><img className='header-img' src={instagramImg} alt="instagram" /></a>
-        <a href="/#"><img className='header-img'src={telegramImg} alt="telegram" /></a>
-        <a href="/#"><img className='header-img' src={facebookImg} alt="facebook" /></a>
-      </div>
-      <div className="app-container">
-
-      <div className="photo-slider-box">
-                <div className="photo-slider">
-                <Carousel  interval="1000" transitionTime="1000" infiniteLoop>
-                    <div>
-                        <img src={Img_2} alt="" />
-                        <button onClick={navigationHandler} className='legend'>Оформити замовлення</button>
-                    </div>
-                    <div>
-                        <img src={Img_3} alt="" />
-                        <button onClick={navigationHandler} className='legend'>Оформити замовлення</button>
-                    </div>
-                    <div>
-                    <img src={Img_4} alt="" />
-                    <button onClick={navigationHandler} className='legend'>Оформити замовлення</button>
-                    </div>
-                    <div>
-                         <img src={Img_5} alt="" />
-                         <button onClick={navigationHandler} className='legend'>Оформити замовлення</button>
-                    </div>
-                    
-                </Carousel>
-            </div>
-            </div>
-      <div className="side-panel">
-
-      <div className="content">
-        <h1 className='content__title'>Відпарювач RAF SKY RS7307 </h1>
-        <div className="content__prise">850₴ <br /> <span className="last_prise">1100₴</span> </div>
-        <div className='content__econom'>Економія 250 грн </div>
-        <button onClick={navigationHandler} className='content_button'>Оформити замовлення</button>
-
-      </div>
-      <div className="description">
-            <DescriptionTabs/>
-      </div>
-            </div>
-    </div>
-    </div>
-  )
+	const navigate = useNavigate()
+	const navigationHandler = () => {
+		navigate('/form')
+	}
+	return (
+		<div>
+			<div className='app-container'>
+				<div className='photo-slider-box'>
+					<div className='photo-slider'>
+						<Carousel interval='1000' transitionTime='1000' infiniteLoop>
+							<div>
+								<img src={Img_2} alt='' />
+								<button onClick={navigationHandler} className='legend'>
+									Оформити замовлення
+								</button>
+							</div>
+							<div>
+								<img src={Img_3} alt='' />
+								<button onClick={navigationHandler} className='legend'>
+									Оформити замовлення
+								</button>
+							</div>
+							<div>
+								<img src={Img_4} alt='' />
+								<button onClick={navigationHandler} className='legend'>
+									Оформити замовлення
+								</button>
+							</div>
+							<div>
+								<img src={Img_5} alt='' />
+								<button onClick={navigationHandler} className='legend'>
+									Оформити замовлення
+								</button>
+							</div>
+						</Carousel>
+					</div>
+				</div>
+				<div className='side-panel'>
+					<div className='content'>
+						<h1 className='content__title'>Відпарювач RAF SKY RS7307 </h1>
+						<div className='content__prise'>
+							850₴ <br /> <span className='last_prise'>1100₴</span>{' '}
+						</div>
+						<div className='content__econom'>Економія 250 грн </div>
+						<button onClick={navigationHandler} className='content_button'>
+							Оформити замовлення
+						</button>
+					</div>
+					<div className='description'>
+						<DescriptionTabs />
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Main
